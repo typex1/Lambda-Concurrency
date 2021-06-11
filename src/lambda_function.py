175eba53f53011f4fcb75a5b3f28852784367d0a
Spiess <fspiess@147ddaa45603.ant.amazon.com>
2021-06-11 14:38:21 +0200

    update

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,14 +2,16 @@ import os
 import time
 
 def lambda_handler(event, context):
-    print("event contents: " + str(event))
-    # print("number of messages in event: "+str(len(event['Records'])))
+    event_contents=str(event)
+    print("event contents: " + event_contents)
+    if 'Records' in event_contents:
+        print("number of messages in event: "+str(len(event['Records'])))
     os.system("hostid > /tmp/output.txt")
     os.system("whoami >> /tmp/output.txt")
     fp = open('/tmp/output.txt', "r")
     output = fp.read()
     fp.close()
-    print("XXX Hostid: "+output)
+    print("Hostid and user: "+output)
     time.sleep(3)
     print("Done.")
 
