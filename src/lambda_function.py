4babb93d6921790b0f794d4da60eb72407bdeb8b
typex1 <8014314+typex1@users.noreply.github.com>
2022-03-07 18:01:16 +0100

    Correct indentation in Lambda Python code

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,22 +4,23 @@ import time
 
 def lambda_handler(event, context):
     event_contents=str(event)
-    #print("event contents: " + event_contents)
+    print("event contents: " + event_contents)
     print("Lambda function memory limits in MB:", context.memory_limit_in_mb)
     if 'Records' in event_contents:
         print("number of messages in event: "+str(len(event['Records'])))
-    records=event['Records']
-    for record in records:
-        body = record['body']
+        records=event['Records']
+        if 'body' in event_contents:
+            for record in records:
+                body = record['body']
         
-        #$RANDOM: random integer in range 0 to 32767 (half of it: 16383)
+                #$RANDOM: random integer in range 0 to 32767 (half of it: 16383)
         
-        ##############
-        # uncomment the next 3 lines if you want to test how Lambda handles SQS messages which cause an error:
-        # value=int((body.split()[2]))
-        # if value > 214230:
-        #    raise ValueError('value {} too high'.format(value)) 
-        ##############
+                ##############
+                # uncomment the next 3 lines if you want to test how Lambda handles SQS messages which cause an error:
+                # value=int((body.split()[2]))
+                # if value > 214230:
+                #    raise ValueError('value {} too high'.format(value)) 
+                ##############
         
     os.system("hostid > /tmp/output.txt")
     fp = open('/tmp/output.txt', "r")
